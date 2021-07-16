234213ceae928b524a21dd4343df3a06344ed8de
riansco14 <rian.rabelo@ufrpe.br>
2021-07-16 04:24:50 -0300

    add styled components e tema global

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Container, Title } from './styles'
 
 export function Dashboard() {
     return (
-        <View>
-            <Text>Dashboard</Text>
-        </View>
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
     )
 }
 
