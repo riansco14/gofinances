ec1ebc399b61357a018557a15b5467470f6514d4
riansco14 <rian.rabelo@ufrpe.br>
2021-07-29 05:43:22 -0300

    Fix Datas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -3,11 +3,9 @@ import React, { useCallback, useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native'
 import { HighlightCard } from '../../components/HighlightCard'
 import { Transaction, TransactionCard } from '../../components/TransactionCard'
-import { categories } from '../../utils/categories'
 import { Container, Header, Icon, Image, User, UserContainer, UserHello, UserInfo, UserName, HighlightCardScroll, Transactions, Title, TransactionList, LogoutButton, LoadingContainer } from './styles'
 import { ActivityIndicator } from 'react-native'
 import theme from '../../global/styles/theme'
-import { types } from '@babel/core'
 
 export interface TransactionOverride extends Transaction {
     id: string
@@ -37,26 +35,49 @@ export function Dashboard() {
     let saidasTotal = 0
     let somaTotal = 0
 
-    function dateFormatHighLightCard(transacoes: TransactionOverride[], type: 'up' | 'down' ) {
-        const ultimaTransacao = new Date(Math.max.apply(Math, transacoes
-            .filter((item: TransactionOverride) => item.transacaoTipo === type)
-            .map((item: TransactionOverride) => new Date(item.date).getTime()) ))
-        
-        const ultimaTransacaoFormatted = Intl.DateTimeFormat('pt-BR', {
-            day: '2-digit',
-            month: '2-digit',
-            year: '2-digit'
-        }).format(ultimaTransacao)
+    function resetValues() {
+        entradasTotal = 0
+        saidasTotal = 0
+        somaTotal = 0
+    }
 
-        
-        return `${ultimaTransacao.getDate()} de ${ultimaTransacao.toLocaleString('pt-BR', {month: 'long'})}`
+    function getLastTransactionDate(
+        collection: TransactionOverride[],
+    ) {
+        if (collection.length <= 0)
+            return '-'
+
+        const lastTransaction = new Date(
+            Math.max.apply(Math, collection
+                .map(transaction => {
+                    return new Date(transaction.date).getTime()
+                })))
+
+        console.log(lastTransaction);
+
+        return `${lastTransaction.getDate()} de ${lastTransaction.toLocaleString('pt-BR', { month: 'long' })}`;
+    }
+
+    function dateFormatHighLightCard(transacoes: TransactionOverride[], type: 'up' | 'down') {
+        const ultimaTransacao = new Date(
+            Math.max.apply(Math, transacoes
+                .filter((item: TransactionOverride) => item.transacaoTipo === type)
+                .map((item: TransactionOverride) => new Date(item.date).getTime())))
+
+
+        return `${ultimaTransacao.getDate()} de ${ultimaTransacao.toLocaleString('pt-BR', { month: 'long' })}`
     }
 
     async function loadTransacao() {
         setIsLoading(true)
+        resetValues()
         const dataKey = "@gofinances:transacaos"
         const response = await AsyncStorage.getItem(dataKey)
         const transacoes = response ? JSON.parse(response) : []
+        console.log(transacoes);
+
+        const transacoesEntrada = transacoes.filter((item: TransactionOverride) => item.transacaoTipo === "up")
+        const transacoesSaida = transacoes.filter((item: TransactionOverride) => item.transacaoTipo === "down")
 
         const transacoesFormatted: TransactionOverride[] = transacoes.map(
             (item: TransactionOverride) => {
@@ -88,10 +109,10 @@ export function Dashboard() {
 
             })
         setData(transacoesFormatted)
-        
-        const ultimaTransacaoEntrada = dateFormatHighLightCard(transacoes, 'up')
-        const ultimaTransacaoSaida = dateFormatHighLightCard(transacoes, 'down')
-        
+
+        const ultimaTransacaoEntrada = getLastTransactionDate(transacoesEntrada)
+        const ultimaTransacaoSaida = getLastTransactionDate(transacoesSaida)
+
         somaTotal = entradasTotal - saidasTotal
         setHighlightData({
             entradas: {
@@ -113,7 +134,7 @@ export function Dashboard() {
                     currency: "BRL",
                     style: "currency"
                 }),
-                ultimaTransacao:  `01 a ${ultimaTransacaoSaida}`
+                ultimaTransacao: `01 a ${ultimaTransacaoSaida}`
             }
 
         })
